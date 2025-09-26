f6e1902abd44cba5b8e09e3dd13ef070e9c8721a
NguyenBich21 <145450978+NguyenBich21@users.noreply.github.com>
2025-09-26 23:31:57 +0700

    Delete datawarehouse directory

--- datawarehouse/dim_actor.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE TABLE `single-healer-463211-e8.olap_sakila.dim_actor` AS
-SELECT
-    ROW_NUMBER() OVER () AS actor_key,
-    a.actor_id,
-    a.first_name,
-    a.last_name
-FROM `single-healer-463211-e8.stg_sakila.actor` a;
--- datawarehouse/film_to_actor_bridge.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TABLE `single-healer-463211-e8.olap_sakila.film_to_actor_bridge` AS
-SELECT
-    ROW_NUMBER() OVER () AS film_to_actor_bridge_key,
-    f.film_id AS film_key,
-    a.actor_id AS actor_key
-FROM `single-healer-463211-e8.stg_sakila.film_actor` fa
-JOIN `single-healer-463211-e8.stg_sakila.film` f ON fa.film_id = f.film_id
-JOIN `single-healer-463211-e8.stg_sakila.actor` a ON fa.actor_id = a.actor_id;
